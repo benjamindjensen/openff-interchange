1ca4fe90859d1f9a019715efbd4aed2736c8fa7b
Matthew W. Thompson <mattwthompson@protonmail.com>
2024-08-19 12:32:51 -0500

    INTEROP: Properly support reaction field electrostatics in OpenMM
    Co-authored-by: Joao Morado <joaomorado@gmail.com>

--- openff/interchange/_tests/unit_tests/interop/openmm/test_nonbonded.py
+++ openff/interchange/_tests/unit_tests/interop/openmm/test_nonbonded.py
@@ -115,6 +115,39 @@ class TestCutoffElectrostatics:
 
 
 @skip_if_missing("openmm")
+class TestCutoffElectrostatics:
+    @pytest.mark.parametrize("combine", [True, False])
+    def test_export_reaction_field_electrostatics(
+        self,
+        sage,
+        basic_top,
+        combine,
+    ):
+        import openmm
+        import openmm.unit
+
+        out = sage.create_interchange(basic_top)
+
+        assert out.box is not None
+        out["Electrostatics"].periodic_potential = "reaction-field"
+
+        out["Electrostatics"].cutoff = out["vdW"].cutoff
+
+        system = out.to_openmm(combine_nonbonded_forces=combine)
+
+        for force in system.getForces():
+            if isinstance(force, openmm.NonbondedForce):
+                assert 0.9 == force.getCutoffDistance() / openmm.unit.nanometer
+
+                # Not testing non-periodic case
+                assert force.getNonbondedMethod() == openmm.NonbondedForce.CutoffPeriodic
+
+                break
+        else:
+            pytest.fail("Found no `NonbondedForce`")
+
+
+@skip_if_missing("openmm")
 class TestEwaldSettings:
     @pytest.mark.parametrize("lj_method", ["cutoff", "pme"])
     def test_set_ewald_tolerance(self, sage, basic_top, lj_method):
